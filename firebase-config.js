fa009a50a30cf6a0424a661830e60bdd8481dec0
Fk-edits <fiqkirhbw@gmail.com>
2026-07-07 19:20:59 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,15 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBVWaHveZgGgcAcgojBMDmDdu1fdeJBgU4",
-  authDomain: "danfer-8bfd4.firebaseapp.com",
-  projectId: "danfer-8bfd4",
-  storageBucket: "danfer-8bfd4.firebasestorage.app",
-  messagingSenderId: "825811670471",
-  appId: "1:825811670471:web:48bf9fa767bedee21075e2",
-  measurementId: "G-Y888P5969S"
+  apiKey: "AIzaSyDDceDGKyu8tyZ_ZLPefkL3ElzVNHfQmN4",
+  authDomain: "sass-school-ea659.firebaseapp.com",
+  projectId: "sass-school-ea659",
+  storageBucket: "sass-school-ea659.firebasestorage.app",
+  messagingSenderId: "941464239698",
+  appId: "1:941464239698:web:ea1fb74408bca4fc95698b",
+  measurementId: "G-VPKNRKCHTR"
 };
 
 const app = initializeApp(firebaseConfig);
